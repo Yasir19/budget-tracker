f34c1856a91024b85655f1bfbbc6ce1ad7323a7d
yasir19 <yasir.habboo@gmail.com>
2022-03-27 01:38:34 -0500

    add service worker

--- public/sw.js
+++ public/sw.js
@@ -21,11 +21,69 @@ const FILES_TO_CACHE = [
 // Install the service worker
 self.addEventListener('install', function (e){
 e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then(cache => {
         console.log('installing cache : ' + CACHE_NAME)
         return cache.addAll(FILES_TO_CACHE)
-
     })
-)
-
+    )
+    self.skipWaiting();
 })
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('acttivate', function(e) {
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== VERSION) { 
+                        console.log('Removing old cache', key);
+                        return caches.delete(key)
+                    }
+                })
+            )
+        })
+    )
+    self.clients.claim();
+})
+
+// Intercept fetch requests
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+      evt.respondWith(
+        caches
+          .open(VERSION)
+          .then(cache => {
+            return fetch(evt.request)
+              .then(response => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(err => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+          })
+          .catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      fetch(evt.request).catch(function() {
+        return caches.match(evt.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (evt.request.headers.get('accept').includes('text/html')) {
+            // return the cached home page for all requests for html pages
+            return caches.match('/');
+          }
+        });
+      })
+    );
+  });
+  
